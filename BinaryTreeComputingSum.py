b3fc3f99e3f410a45762111ce3adc544739f62b0
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2023-01-14 23:52:31 +0800

    Binary Search Tree Node Class

--- BinaryTreeComputingSum.py
+++ BinaryTreeComputingSum.py
@@ -4,3 +4,15 @@ print("\t\t\t\t\t\t***BEVERLY ANN L. RODRIGUEZ***\n ")
 # BINARY TREE ACTIVITY
 # This program aims to test the COMPUTING SUM method.
 # The list will contain numbers instead of letters. 
+
+
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    # The value already exist.
+    def add_child(self, data):
+        if data == self.data:
+            return
