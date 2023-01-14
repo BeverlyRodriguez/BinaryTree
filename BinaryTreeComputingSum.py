b6d63d398f464f2717c4429d196f831fd57bd561
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2023-01-14 23:50:22 +0800

    Adding Comments

--- BinaryTreeComputingSum.py
+++ BinaryTreeComputingSum.py
@@ -1,2 +1,6 @@
 print("\n\t\t\t\t\t\t********PROGRAMMED BY:******** ")
 print("\t\t\t\t\t\t***BEVERLY ANN L. RODRIGUEZ***\n ")
+
+# BINARY TREE ACTIVITY
+# This program aims to test the COMPUTING SUM method.
+# The list will contain numbers instead of letters. 
