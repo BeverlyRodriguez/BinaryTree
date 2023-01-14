de4694678ece6a1afec8dbe6f396d7e0d2c4bd0e
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2023-01-14 20:52:52 +0800

    Directions Comment

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,2 +1,5 @@
 print("\n\t\t ********PROGRAMMED BY:******** ")
 print("\t\t ***BEVERLY ANN L. RODRIGUEZ***\n ")
+
+# BINARY TREE ACTIVITY
+# Creating a demo using the letters in your fullname as the content of the binary tree.
