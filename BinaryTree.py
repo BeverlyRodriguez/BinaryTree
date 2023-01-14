2938f1beabf61dfae377df13dbccd9177ac513ec
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2023-01-14 21:03:24 +0800

    If Else and comments

--- BinaryTree.py
+++ BinaryTree.py
@@ -10,7 +10,20 @@ class BinarySearchTreeNode:
         self.data = data
         self.left = None
         self.right = None
+    
 
+    # The value already exist.
     def add_child(self, data):
         if data == self.data:
-            return # node already exist
+            return
+
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
