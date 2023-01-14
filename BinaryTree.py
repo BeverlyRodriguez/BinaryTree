bf156231d3a7e8da7dd9a66e4a0f1ebbcf26ce9e
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2023-01-14 20:57:42 +0800

    Creating Binary SearchTreeNode Class

--- BinaryTree.py
+++ BinaryTree.py
@@ -3,3 +3,14 @@ print("\t\t ***BEVERLY ANN L. RODRIGUEZ***\n ")
 
 # BINARY TREE ACTIVITY
 # Creating a demo using the letters in your fullname as the content of the binary tree.
+
+
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return # node already exist
