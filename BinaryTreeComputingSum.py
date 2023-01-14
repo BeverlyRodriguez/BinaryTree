45c34a7f25cad58fb8240ec5143732d21bf7f58f
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2023-01-14 23:55:17 +0800

    In Order Traversal Method

--- BinaryTreeComputingSum.py
+++ BinaryTreeComputingSum.py
@@ -47,3 +47,17 @@ class BinarySearchTreeNode:
                 return self.right.search(val)
             else:
                 return False
+
+    # In-order traversal
+    # Returning list of elements in specific order.
+    def in_order_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
